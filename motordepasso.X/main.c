acd06b5531ab3e7ad9118dfc1cbeeb6e6a143390
20185070 <20185070@DSN1271158610.cfp127aluno.local>
2021-06-02 16:23:40 -0300

    Alterações do Carlos 2

--- motordepasso.X/main.c
+++ motordepasso.X/main.c
@@ -3,13 +3,40 @@
 #include "motorpasso.h"
 #include "delay.h"
 
-void main (void)
+#define  S1   PORTDbits.RD0
+#define  S2   PORTDbits.RD1
+
+void main (void)  
 {
-    motorpasso_init();
+    
+    int estado = 0;
     
     while( 1 )
     {
-        motorpasso(48, 100);
+        switch(estado)
+        {
+            case 0:
+                estado = 1;
+                break;
+            case 1:
+                if (S1 == 1)
+                    estado = 2;
+                if (S2 == 1)
+                    estado = 3;
+                break;
+            case 2:
+                motorpasso_init();
+                motorpasso(48, 100);
+                if (S2 ==1)
+                estado = 1;
+                break;
+            case 3:
+                motorpasso_init();
+                motorantpasso(48, 100);
+                if (S2 == 1)
+                estado = 1;
+                break;
+        }
     }
     return;
 }
